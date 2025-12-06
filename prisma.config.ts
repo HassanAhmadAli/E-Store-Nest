9111f4f127cf16c73df6a53bfca1f1e7425001b7
Hassan <hassan.ali.36900@outlook.com>
2025-12-06 20:56:06 +0300

    ok

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 export default defineConfig({
-  schema: "prisma/scehma.prisma",
+  schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
